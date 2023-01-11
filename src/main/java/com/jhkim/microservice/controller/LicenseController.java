2c7256690f29bd2186336d9c94f3fa8ecaafedb0
김재홍 <jhkim@onthelive.kr>
2023-01-11 16:53:08 +0900

    controller @GetMapping추가

--- src/main/java/com/jhkim/microservice/controller/LicenseController.java
+++ src/main/java/com/jhkim/microservice/controller/LicenseController.java
@@ -1,9 +1,26 @@
 package com.jhkim.microservice.controller;
 
+import com.jhkim.microservice.model.License;
+import com.jhkim.microservice.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/organization/{organizationId}/license")
 public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping("/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+                                              @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLisence(licenseId, organizationId);
+
+        return ResponseEntity.ok(license);
+    }
 }
