c0a3747398ee2819eeef5e99325d6c6522af3305
김재홍 <jhkim@onthelive.kr>
2023-01-11 17:01:35 +0900

    controller service기능 추가

--- src/main/java/com/jhkim/microservice/controller/LicenseController.java
+++ src/main/java/com/jhkim/microservice/controller/LicenseController.java
@@ -4,10 +4,7 @@ import com.jhkim.microservice.model.License;
 import com.jhkim.microservice.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/organization/{organizationId}/license")
@@ -22,5 +19,26 @@ public class LicenseController {
         License license = licenseService.getLisence(licenseId, organizationId);
 
         return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+    @DeleteMapping("/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
+                                                @PathVariable("licenseId") String licenseId) {
+
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
     }
 }
